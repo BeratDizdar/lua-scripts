b3afb764986d5e8622177d652e011f40f923d1b9
BeratDizdar <139409706+BeratDizdar@users.noreply.github.com>
2024-12-07 01:38:12 +0300

    Add files via upload

---
+++ source/main.lua
@@ -0,0 +1,31 @@
+---------------------
+--[ Import Modules]--
+---------------------
+
+require("struct")
+require("progressbar")
+require("redef")
+require("utils")
+
+----------------------
+--[ Load Functions ]--
+----------------------
+
+gameStates = {
+    menu = 1,
+    ingame = 2,
+    options = 3
+}
+local StateManager = require("state_manager")
+StateManager.registerStateModule(gameStates.menu, "menu")
+StateManager.registerStateModule(gameStates.ingame, "ingame")
+StateManager.registerStateModule(gameStates.options, "options")
+StateManager.setState(gameStates.menu)
+
+----------------------
+--[ MAIN FUNCTIONS ]--
+----------------------
+
+function love.load() end
+function love.update(dt) StateManager.update(dt) end
+function love.draw() StateManager.draw() end
